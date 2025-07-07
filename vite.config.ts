1d4a2cea5234bf7093e65c4804247eda4eb36d72
Ramyasrilakshmigubbala <ramyagubbala12@gmail.com>
2025-07-07 16:07:57 +0530

    Final build and cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { ConfigEnv, UserConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: '@', replacement: path.resolve(__dirname, 'src') }
+      ]
     },
-  },
-}));
+    server: {
+      port: 3000,
+      open: true,
+      host: true, // needed for docker
+      strictPort: true
+    },
+    preview: {
+      port: 3000,
+      open: true,
+      strictPort: true
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+      manifest: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'react-vendor': ['react', 'react-dom', 'react-router-dom']
+          }
+        }
+      }
+    }
+  }
+});
+
+
+
